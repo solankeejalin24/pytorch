c9eda0135f46b178b75613ad2780d4c510f89e92
Edward Z. Yang <ezyang@meta.com>
2024-04-15 06:12:06 -0700

    Refactor all top level usages of record_shapeenv_event to ShapeEnv class
    This ensures that first argument to record_shapeenv_event is a ShapeEnv
    so we can appropriately short circuit when recording is not in progress.
    
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>
    
    ghstack-source-id: 508352a4043994f5cab68e9df8a5e5f53ea58d2d
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/123735

--- torch/_export/serde/serialize.py
+++ torch/_export/serde/serialize.py
@@ -1385,8 +1385,7 @@ class GraphModuleDeserializer(metaclass=Final):
                         self.shape_env.add_var_to_val(sym, hint)
 
                     if vr := self.symbol_name_to_range.get(val.expr_str):
-                        symbolic_shapes._constrain_symbol_range(
-                            self.shape_env,
+                        self.shape_env._constrain_symbol_range(
                             sym,
                             compiler_min=vr.lower,  # type: ignore[arg-type]
                             compiler_max=vr.upper,  # type: ignore[arg-type]
@@ -1401,8 +1400,7 @@ class GraphModuleDeserializer(metaclass=Final):
                         if s.name not in self.symbol_name_to_symbol:
                             self.symbol_name_to_symbol[s.name] = s
                         if vr := self.symbol_name_to_range.get(s.name):
-                            symbolic_shapes._constrain_symbol_range(
-                                self.shape_env,
+                            self.shape_env._constrain_symbol_range(
                                 s,
                                 compiler_min=vr.lower,  # type: ignore[arg-type]
                                 compiler_max=vr.upper,  # type: ignore[arg-type]
