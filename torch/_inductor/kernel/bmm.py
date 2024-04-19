762991a962a387ee233368e6a0f59e0ec002f599
Kai Londenberg <klondenberg@meta.com>
2024-04-19 20:16:29 +0200

    [Inductor cutlass backend] Add bmm support
    Add support for bmm ( batch matrix multiply ) op through the Cutlass backend.
    
    Test Plan:
     * CI
     * Added test in test_cutlass_backend.py
    
    ghstack-source-id: c8c9572d60986b64bdae232b9096597bb83a2d9a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/121734

--- torch/_inductor/kernel/bmm.py
+++ torch/_inductor/kernel/bmm.py
@@ -1,3 +1,5 @@
+import logging
+
 import torch
 
 from ..lowering import register_lowering
@@ -6,10 +8,17 @@ from ..select_algorithm import (
     ExternKernelChoice,
     TritonTemplate,
 )
-from ..utils import ceildiv as cdiv, use_aten_gemm_kernels, use_triton_template
+from ..utils import (
+    ceildiv as cdiv,
+    use_aten_gemm_kernels,
+    use_cutlass_template,
+    use_triton_template,
+)
+from .mm import _is_static_problem
 
 from .mm_common import addmm_epilogue, mm_args, mm_configs, mm_options
 
+log = logging.getLogger(__name__)
 aten = torch.ops.aten
 
 
@@ -99,6 +108,15 @@ def tuned_bmm(mat1, mat2, *, layout=None):
                 layout=layout,
                 **mm_options(config, m, n, k, layout),
             )
+    static_shape, is_nonzero = _is_static_problem([mat1, mat2], layout)
+    if static_shape and is_nonzero and use_cutlass_template(layout, m, n, k):
+        from ..codegen.cuda.gemm_template import CUTLASSGemmTemplate
+
+        CUTLASSGemmTemplate.add_cutlass_gemm_choices(choices, layout, [mat1, mat2])
+
+    if len(choices) == 0:
+        log.warning("No choices for GEMM, using ATen backend as fallback")
+        choices.append(aten_bmm.bind((mat1, mat2), layout))
 
     return autotune_select_algorithm("bmm", choices, [mat1, mat2], layout)
 
