c624b9cc96807ef97483db5d1443f877bea327ab
Wanchao Liang <wanchaol@users.noreply.github.com>
2024-04-18 22:06:03 -0700

    [tp] add kwargs support to prepare_module_input
    as titled, this PR adds kwargs support to PrepareModuleInput style,
    where there might be modules who have only kwargs inputs but no
    positional args, so we should support this
    
    ghstack-source-id: 8c78364d6b6517de4e7748012bde9dfdc8f2f634
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/124114

--- test/distributed/tensor/parallel/test_tp_style.py
+++ test/distributed/tensor/parallel/test_tp_style.py
@@ -246,6 +246,39 @@ class TensorParallelStyleTest(DTensorTestBase):
         self.assertEqual(output.shape, (self.world_size * 2, 8 // self.world_size))
 
     @with_comms
+    def test_prepare_module_kwargs_input(self):
+        mesh = init_device_mesh(self.device_type, (self.world_size,))
+
+        class TestModule(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.linear = torch.nn.Linear(8, 8)
+
+            def forward(self, x, *, y, z=2):
+                return self.linear(x) + y + z
+
+        test_mod = TestModule().to(self.device_type)
+        # Raise assertion error if module inputs and input_layouts do not have same length.
+        prepare_inps_simple = PrepareModuleInput(
+            input_kwarg_layouts={"y": Shard(0)},
+            desired_input_kwarg_layouts={"y": Replicate()},
+        )
+        parallelize_module(
+            test_mod.linear, mesh, ColwiseParallel(use_local_output=False)
+        )
+        parallelize_module(test_mod, mesh, prepare_inps_simple)
+
+        comm_mode = CommDebugMode()
+        with comm_mode:
+            output = test_mod(
+                torch.randn(4, 8, device=self.device_type),
+                y=torch.ones(1, 8, device=self.device_type),
+            )
+
+        self.assertEqual(comm_mode.get_total_counts(), 1)
+        self.assertEqual(output.shape, (4, 8))
+
+    @with_comms
     def test_prepare_module_output(self):
         mesh = init_device_mesh(self.device_type, (self.world_size,))
 
